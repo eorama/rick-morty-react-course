3abb1b1c3b792f419155aa6e597354d02ed4b0e6
eorama <eliezerorama15@gmail.com>
2023-05-17 22:52:03 -0300

    actualización app react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import imageRickMorty from './img/rick-morty.png'
-import './App.css';
-import { useState } from 'react';
-import Characters from './components/Characters';
+import imageRickMorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
-  const[characters, setCharacters] = useState(null);
+  const [characters, setCharacters] = useState(null);
 
   const reqApi = async () => {
-    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const api = await fetch("https://rickandmortyapi.com/api/character");
     const characterApi = await api.json();
-    
+
     setCharacters(characterApi.results);
   };
 
@@ -18,14 +18,15 @@ function App() {
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
         {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters}/>
+          <Characters characters={characters} setCharacters={setCharacters} />
         ) : (
           <>
             <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+            <button onClick={reqApi} className="btn-search">
+              Buscar mas Personajes
+            </button>
           </>
         )}
-        
       </header>
     </div>
   );
